e84cbd9adcc5b2ccdfedb2b3a0090446a562a422
CJ Jessett <email": null>
2016-08-10 14:10:26 -0500

    views, gemfile

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,12 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :votable
   has_many :answers
   belongs_to :user
+
+  def username
+    return user.username
+  end
+
+  def points
+    votes.sum(:value)
+  end
 end
