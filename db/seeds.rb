b07cb0bd38d2cb735e6c4694ffaca2a5855751e2
CJ Jessett <email": null>
2016-08-10 15:55:28 -0500

    views stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.all.destroy
-Question.all.destroy
+User.destroy_all
+Question.destroy_all
 
 cj = User.create(username: 'cj', password: 'test')
 Question.create(title: 'how old is shaq?', content: 'how old is he?', user: cj)
