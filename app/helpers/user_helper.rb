a2caaa9e46de664f24229579b4fae3764f37736d
CJ Jessett <email": null>
2016-08-10 23:27:13 -0500

    update question views to handle vote status

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,21 @@ helpers do
   def current_user
     User.find_by_id session[:user_id]
   end
+
+  def user_vote_status(question)
+    if current_user
+      val = current_user.votes.find_or_initialize_by(votable: question).value
+      case val
+      when 1
+        erb :'questions/_up'
+      when -1
+        erb :'questions/_down'
+      when 0
+        erb :'questions/_basic'
+      end
+    else
+        erb :'questions/_basic'
+    end
+  end
 end
+
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
 class Question < ActiveRecord::Base
-  # Remember to create a migration!
+
   has_many :votes, as: :votable
   has_many :answers
   belongs_to :user
 
   def username
-    return user.username
+    user.username
   end
 
   def points
