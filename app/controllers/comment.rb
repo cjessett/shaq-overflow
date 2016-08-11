a3abe4e1696ac173d47822366f66226788204592
Sean Witt <Sean.robin.witt@gmail.com>
2016-08-11 17:56:51 -0500

    comments on fleek

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -9,3 +9,15 @@ post '/questions/:question_id/answer/:answer_id/comment/new' do
   end
 
 end
+
+
+post '/questions/:question_id/comment/new' do
+  question = Question.find(params[:question_id])
+  comment = Comment.new(content: params[:comment], commentable: question, user_id: session[:user_id])
+  if comment.save
+    redirect to "/questions/#{params[:question_id]}"
+  else
+    nooo
+  end
+
+end
